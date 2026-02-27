7d5b61ce5ce77f74a2574f196c656290da6e1b0a
Tam <5542463+Tam-d@users.noreply.github.com>
2026-02-27 04:38:30 +0000

    Add prompt and input handling

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,27 @@
+import { createInterface } from "node:readline";
+
 export function cleanInput(input: string): string[] {
   return input.toLowerCase().trim().split(/\s+/);
+}
+
+export function startREPL() {
+
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > "
+    });
+
+    rl.prompt();
+
+    rl.on('line', (input:string) => {
+        if(input.length === 0)
+            rl.prompt;
+
+        let result = cleanInput(input);
+
+        console.log(`Your command was: ${result[0]}`);
+
+        rl.prompt();
+    }); 
 }
