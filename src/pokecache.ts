352d9029572274aeda62bb8f3d34fb0e2a1ae904
Tam <5542463+Tam-d@users.noreply.github.com>
2026-03-02 21:11:51 +0000

    Add cache implementation

---
+++ src/pokecache.ts
@@ -0,0 +1,63 @@
+export type CacheEntry<T> = {
+    createdAt: number,
+    val: T
+};
+
+export class Cache {
+    #cache = new Map<string, CacheEntry<any>>();
+    #reapIntervalId: NodeJS.Timeout | undefined = undefined;
+    #interval: number;
+
+    constructor(num: number) {
+        this.#interval = num;
+        this.#startReapLoop();
+    }
+
+
+    add<T>(key: string, val: T) {
+        this.#cache.set(
+            key, 
+            {
+                createdAt: Date.now(), 
+                val: val
+            }
+        );
+    }
+
+    get<T>(key: string) : T {
+        return this.#cache.get(key)?.val;
+    }
+
+    #reap() {
+        for(const key in this.#cache.keys()) {
+            const cacheEntry = this.#cache.get(key)?.val;
+
+            if(cacheEntry.createdAt < Date.now() - this.#interval) {
+                this.#cache.delete(key);
+            }
+        }
+    }
+
+    #startReapLoop() {
+        try{
+            this.#reapIntervalId = setInterval(this.#reap, this.#interval)
+        }
+        catch(e) {
+            console.log(`Error in startReapLoop: ${e}`);
+        }
+        
+    }
+
+    stopReapLoop() {
+
+        try {
+            clearInterval(this.#reapIntervalId);
+            this.#reapIntervalId = undefined;
+        }
+        catch(e) {
+            console.log(`Error attempting to stop interval with id ${this.#reapIntervalId}`);
+        }
+        
+    }
+
+}
